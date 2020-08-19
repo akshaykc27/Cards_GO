8206e3b7d3507ebce315700d3071bb524a4d0749
akshaykc27 <akshaykc27@gmail.com>
2020-08-19 12:14:59 +0530

    Refactor. print function in deck.go

--- deck.go
+++ deck.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 //Create a new type  "deck"
 //which is slice of strings
 
-type deck []string 
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
